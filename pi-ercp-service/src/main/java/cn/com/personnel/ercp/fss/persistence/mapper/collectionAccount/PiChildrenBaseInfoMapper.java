dfdcc26d6d57e4e50f55747777f134964582a3c1
dingyingying123 <471259661@qq.com>
2021-11-20 22:34:02 +0800

    demo

---
+++ pi-ercp-service/src/main/java/cn/com/personnel/ercp/fss/persistence/mapper/collectionAccount/PiChildrenBaseInfoMapper.java
@@ -0,0 +1,7 @@
+package cn.com.personnel.ercp.fss.persistence.mapper.collectionAccount;
+
+import cn.com.personnel.ercp.fss.persistence.entity.collectionAccount.PiChildrenBaseInfo;
+import cn.com.personnel.springboot.framework.core.persistence.BaseMapper;
+
+public interface PiChildrenBaseInfoMapper extends BaseMapper<PiChildrenBaseInfo> {
+}
