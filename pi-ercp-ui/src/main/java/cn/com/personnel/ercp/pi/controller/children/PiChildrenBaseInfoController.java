41dd387430b95ccc2d7ebf5e8f8f0834004716be
dingyingying123 <471259661@qq.com>
2021-11-20 23:01:56 +0800

    添加儿童信息接口文档

---
+++ pi-ercp-ui/src/main/java/cn/com/personnel/ercp/pi/controller/children/PiChildrenBaseInfoController.java
@@ -0,0 +1,11 @@
+package cn.com.personnel.ercp.pi.controller.children;
+
+import cn.com.personnel.springboot.framework.core.controller.PageController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/piChildrenBaseInfo")
+public class PiChildrenBaseInfoController extends PageController {
+
+}
